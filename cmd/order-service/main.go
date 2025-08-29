d75501d77f372895014faac72359619fb4e38e8b
Almir <almirwork98@gmail.com>
2025-08-29 15:59:41 +0300

    feat: create main file

---
+++ cmd/order-service/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	cfg := config.Load()
+	logger := log.New(cfg.AppEnv)
+	defer func() {
+		if err := logger.Sync(); err != nil {
+			logger.Error("failed to sync logger", "error", err)
+		}
+	}()
+
+	if err := app.Run(ctx, cfg, logger); err != nil {
+		logger.Panic("failed to run application", "error", err)
+	}
+
+	time.Sleep(100 * time.Millisecond)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GolangDeveloperAlmir/order-service
+
+go 1.25.0
